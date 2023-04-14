3324e691c57c1faa1a9782bd39db420b600eee30
Christian Aquino <cjfaquino@gmail.com>
2023-04-13 22:12:32 -0700

    add password hash

--- routes/sign-up.ts
+++ routes/sign-up.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import bcryptjs from 'bcryptjs';
 import User from '../models/User';
 const signUpRouter = Router();
 
@@ -6,12 +7,16 @@ signUpRouter.get('/', (req, res) => res.render('sign-up-form'));
 
 signUpRouter.post('/', async (req, res, next) => {
   try {
-    const user = new User({
-      username: req.body.username,
-      password: req.body.password,
+    bcryptjs.hash(req.body.password, 10, async (err, hashedPass) => {
+      if (err) throw err;
+
+      const user = new User({
+        username: req.body.username,
+        password: hashedPass,
+      });
+      const result = await user.save();
+      res.redirect('/');
     });
-    const result = await user.save();
-    res.redirect('/');
   } catch (error) {
     return next(error);
   }
