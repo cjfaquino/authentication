87a2cb32d418a6e1aaa73ba0c2794b36b883aa43
Christian Aquino <cjfaquino@gmail.com>
2023-04-13 21:56:56 -0700

    move log in to index

--- routes/log-in.ts
+++ routes/log-in.ts
@@ -2,10 +2,6 @@ import { Router } from 'express';
 import passport from 'passport';
 const logInRouter = Router();
 
-logInRouter.get('/', (req, res) => {
-  res.render('log-in');
-});
-
 logInRouter.post(
   '/',
   passport.authenticate('local', {
