efdbd647c1c0a2f918b5166e4b5e34d060294174
Christian Aquino <cjfaquino@gmail.com>
2023-04-13 21:20:38 -0700

    add routes

--- routes/sign-up.ts
+++ routes/sign-up.ts
@@ -1,6 +1,20 @@
 import { Router } from 'express';
+import User from '../models/User';
 const signUpRouter = Router();
 
-signUpRouter.get('/sign-up', (req, res) => res.render('sign-up-form'));
+signUpRouter.get('/', (req, res) => res.render('sign-up-form'));
+
+signUpRouter.post('/', async (req, res, next) => {
+  try {
+    const user = new User({
+      username: req.body.username,
+      password: req.body.password,
+    });
+    const result = await user.save();
+    res.redirect('/');
+  } catch (error) {
+    return next(error);
+  }
+});
 
 export default signUpRouter;
